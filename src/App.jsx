123e317e3d1e316dcde4a6406a3bb44d1d7cc105
Brian Kinyua <briankinyua0101@gmail.com>
2024-01-18 13:32:38 +0300

    done with redux code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 
-import './App.css'
+
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
 function App() {
   
 
   return (
-    <>
+    <Provider store={store}>
+      <div>
+        <h1>Hello Developer</h1>
+      </div>
       
-    </>
+    </Provider>
   )
 }
 
