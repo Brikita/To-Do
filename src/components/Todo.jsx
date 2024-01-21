9546db9a4435d28d2ea3c2ffc90c582e0f0dabdd
Brian Kinyua <134489011+Brikita@users.noreply.github.com>
2024-01-21 20:25:17 +0300

    Update Todo.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -27,7 +27,7 @@ const Todo = () => {
   return (
     <div className="max-w-4xl mx-auto sm:mt-8 p-4 bg-gray-100 rounded">
       <h2 className="mt-3 mb-6 text-2xl font-bold text-center uppercase">
-        Personal TODO APP
+        Personal TO-DO APP
       </h2>
       <div className="flex items-center mb-4">
         <input
@@ -52,7 +52,7 @@ const Todo = () => {
           <input
             className="flex-grow p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500"
             type="text"
-            placeholder="Search Todos"
+            placeholder="Search To-dos"
             value={searchTerm}
             onChange={(e) => handleSearchChange(e.target.value)}
           />
